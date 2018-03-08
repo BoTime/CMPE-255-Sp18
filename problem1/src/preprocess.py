5b862888984394e6f81ea385e587c8e3bd5c4f71
botime <bo.liu@sjsu.edu>
2018-03-07 23:15:14 -0800

    stemmer

--- problem1/src/preprocess.py
+++ problem1/src/preprocess.py
@@ -8,19 +8,21 @@ from nltk.stem import SnowballStemmer
 max_features = None
 binary = False
 
+
 class LemmaAndStemTokenizer(object):
     def __init__(self):
         self.lemmatizer = WordNetLemmatizer()
         self.stemmer = SnowballStemmer("english")
 
     def __call__(self, doc):
-        return [self.stemmer.stem(t1) for t1 in [self.lemmatizer.lemmatize(t2) for t2 in word_tokenize(doc)]]
+        # return [self.stemmer.stem(t1) for t1 in [self.lemmatizer.lemmatize(t2) for t2 in word_tokenize(doc)]]
+        return [self.stemmer.stem(t) for t in word_tokenize(doc)]
 
 
 vectorizer = TfidfVectorizer(
     stop_words='english',
     analyzer='word',
-    # tokenizer=LemmaAndStemTokenizer()
+    tokenizer=LemmaAndStemTokenizer()
 )
 # vectorizer = CountVectorizer(max_features=max_features, binary=binary)
 
@@ -38,8 +40,8 @@ def get_tf_idf_testing(train_vocabulary, testing_set, vocabulary=None):
     vectorizer = TfidfVectorizer(
         stop_words='english',
         analyzer='word',
-        vocabulary=train_vocabulary
-        # tokenizer=LemmaAndStemTokenizer()
+        vocabulary=train_vocabulary,
+        tokenizer=LemmaAndStemTokenizer()
     )
 
     vectorizer.vocabulary_ = train_vocabulary
